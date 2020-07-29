c0efd6fe54055f6d5de812243e7592488cf23fcd
dogra1598 <dogravishal1598@gmail.com>
2020-07-29 09:18:09 +0530

    starting this project first commit

---
+++ src/shop/components/Products/Product/Product.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Product.css";
+
+const Product = () => {
+  return <div className="Product"></div>;
+}
+
+export default Product;
---
+++ src/shop/components/Products/Products.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Produts.css";
+
+const Produts = () => {
+  return <div className="Produts"></div>;
+}
+
+export default Produts;
